6699b5785dc19344de6e77d4667dda8725672e88
[파인원] 김권수 <kks92155784@gmail.com>
2025-04-09 10:16:04 +0900

    MainScreen, BottomNavigationBar 구현

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,5 +1,9 @@
 import 'package:go_router/go_router.dart';
 import 'package:inf_fl/core/routing/route_paths.dart';
+import 'package:inf_fl/presentation/home/home_screen.dart';
+import 'package:inf_fl/presentation/main/main_screen.dart';
+import 'package:inf_fl/presentation/notifications/notifications_screen.dart';
+import 'package:inf_fl/presentation/profile/profile_screen.dart';
 import 'package:inf_fl/presentation/saved_recipes/screen/saved_recipes_root.dart';
 import 'package:inf_fl/presentation/sign_in/sign_in_screen.dart';
 import 'package:inf_fl/presentation/sign_up/sign_up_screen.dart';
@@ -21,7 +25,7 @@ final router = GoRouter(
       path: RoutePaths.signIn,
       builder:
           (context, state) => SignInScreen(
-            onTapSignIn: () => context.go(RoutePaths.savedRecipes),
+            onTapSignIn: () => context.go(RoutePaths.home),
             onTapSignUp: () => context.go(RoutePaths.signUp),
           ),
     ),
@@ -34,9 +38,53 @@ final router = GoRouter(
             },
           ),
     ),
-    GoRoute(
-      path: RoutePaths.savedRecipes,
-      builder: (context, state) => const SavedRecipesRoot(),
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return MainScreen(
+          body: navigationShell,
+          currentPageIndex: navigationShell.currentIndex,
+          onChangeIndex: (index) {
+            navigationShell.goBranch(
+              index,
+              initialLocation: index == navigationShell.currentIndex,
+            );
+          },
+        );
+      },
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.home,
+              builder: (context, state) => const HomeScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.savedRecipes,
+              builder: (context, state) => const SavedRecipesRoot(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.notifications,
+              builder: (context, state) => const NotificationsScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.profile,
+              builder: (context, state) => const ProfileScreen(),
+            ),
+          ],
+        ),
+      ],
     ),
   ],
 );
---
+++ lib/presentation/main/main_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:inf_fl/ui/color_st.dart';
+
+class MainScreen extends StatelessWidget {
+  final Widget body;
+  final int currentPageIndex;
+  final void Function(int index) onChangeIndex;
+  const MainScreen({
+    super.key,
+    required this.body,
+    required this.currentPageIndex,
+    required this.onChangeIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: body,
+      bottomNavigationBar: NavigationBar(
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+        indicatorColor: Colors.transparent,
+        onDestinationSelected: onChangeIndex,
+        selectedIndex: currentPageIndex,
+        destinations: [
+          const NavigationDestination(
+            icon: Icon(Icons.home_outlined, color: CST.gray4),
+            selectedIcon: Stack(
+              children: [
+                Icon(Icons.home, color: CST.primary40),
+                Icon(Icons.home_outlined, color: CST.primary100),
+              ],
+            ),
+            label: 'Home',
+          ),
+          const NavigationDestination(
+            icon: Icon(Icons.bookmark_border, color: CST.gray4),
+            selectedIcon: Stack(
+              children: [
+                Icon(Icons.bookmark, color: CST.primary40),
+                Icon(Icons.bookmark_border, color: CST.primary100),
+              ],
+            ),
+            label: 'Home',
+          ),
+          const NavigationDestination(
+            icon: Icon(Icons.notifications_outlined, color: CST.gray4),
+            selectedIcon: Stack(
+              children: [
+                Icon(Icons.notifications, color: CST.primary40),
+                Icon(Icons.notifications_outlined, color: CST.primary100),
+              ],
+            ),
+            label: 'Home',
+          ),
+          const NavigationDestination(
+            icon: Icon(Icons.person_outline, color: CST.gray4),
+            selectedIcon: Stack(
+              children: [
+                Icon(Icons.person, color: CST.primary40),
+                Icon(Icons.person_outline, color: CST.primary100),
+              ],
+            ),
+            label: 'Home',
+          ),
+        ],
+      ),
+    );
+  }
+}
